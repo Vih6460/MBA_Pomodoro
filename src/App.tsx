e3b77f9e89ba348d664bfe7ea988876563349229
Vinicius Oliveira <viniciuz6460@gmail.com>
2025-02-09 18:31:08 -0300

    Revert "configs for github pages"
    This reverts commit 35a350b9292ee537d0e6d22302eb1113cb02963f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
 import { Router } from "./Router"
@@ -8,11 +8,11 @@ import { CyclesContextProvider } from "./contexts/CyclesContext"
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <HashRouter>
+      <BrowserRouter>
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
-      </HashRouter>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
